5616454d3d61d62e05becdb39f99a3e2000550db
tkedjp <120151701+tkedjp@users.noreply.github.com>
2023-02-13 09:20:42 +0900

    2nd commit

--- timer_2.py
+++ timer_2.py
@@ -25,31 +25,29 @@ time_select_frame.pack()
 button_frame.pack()
 
 #時間設定
-minute=StringVar()
-minute.set('00')
-second=StringVar()
-second.set('00')
+min=StringVar()
+min_label = tkinter.Label(time_frame, textvariable=min, font=basic_font, fg='black')
+min.set('00')
+min_label.grid(row=1, column=2, padx=5, pady=15, ipadx=4, ipady=4)
 
-#ラベル
-minute_label = tkinter.Label(time_frame, textvariable=minute, font=basic_font, fg='black')
-minute_label.grid(row=1, column=2, padx=5, pady=15, ipadx=4, ipady=4)
 colon_label = tkinter.Label(time_frame, text=':', font=basic_font, fg='black')
 colon_label.grid(row=1, column=3, padx=5, pady=15, ipadx=4, ipady=4)
-second_label = tkinter.Label(time_frame, textvariable=second, font=basic_font, fg='black')
-second_label.grid(row=1, column=4, padx=5, pady=15, ipadx=4, ipady=4)
+
+sec=StringVar()
+sec_label = tkinter.Label(time_frame, textvariable=sec, font=basic_font, fg='black')
+sec.set('00')
+sec_label.grid(row=1, column=4, padx=5, pady=15, ipadx=4, ipady=4)
 
 def Timer():
     # global minute,second
-    times=int(minute.get())*60 + int(second.get())
+    times=int(min.get())*60+int(sec.get())
+    print(times)
 
     while times > -1:
-        minute, second=(times//60, times %60)
-        # hour=0
-        if minute>60:
-            minute=(minute//60, minute%60)
+        minute, second=(times//60, times%60)
 
-        second.set(second)
-        minute.set(minute)
+        sec.set(second)
+        min.set(minute)
 
         time_frame.update()
         time.sleep(1)
@@ -62,16 +60,16 @@ def Timer():
         times -= 1
 
 def three():
-    minute.set("03")
-    second.set("00")
+    min.set("03")
+    sec.set("00")
 
 def four():
-    minute.set("04")
-    second.set("00")
+    min.set("04")
+    sec.set("00")
 
 def five():
-    minute.set("05")
-    second.set("00")
+    min.set("05")
+    sec.set("00")
 
 #時間設定
 stop_button = Button(time_select_frame, text='3分', command=three)
@@ -84,7 +82,7 @@ stop_button.grid(row=1, column=3, padx=5, pady=20, ipadx=3, ipady=3)
 #ボタン
 start_button = Button(button_frame, text='START', command=Timer)
 start_button.grid(row=1, column=1, padx=5, pady=20, ipadx=3, ipady=3)
-reset_button = Button(button_frame, text='RESET')
+reset_button = Button(button_frame, text='STOP')
 reset_button.grid(row=1, column=2, padx=5, pady=20, ipadx=3, ipady=3)
 
 # ウィンドウのループ処理
