4e73ce6a4c57cc5867eb56b8b1ab1e24049476ce
tkedjp <120151701+tkedjp@users.noreply.github.com>
2023-02-13 11:44:13 +0900

    3rd commit

--- timer_2.py
+++ timer_2.py
@@ -1,5 +1,4 @@
 import tkinter
-# from tkinter import ttk
 from tkinter import *
 from tkmacosx import Button
 
@@ -27,7 +26,7 @@ button_frame.pack()
 #時間設定
 min=StringVar()
 min_label = tkinter.Label(time_frame, textvariable=min, font=basic_font, fg='black')
-min.set('00')
+min.set('0')
 min_label.grid(row=1, column=2, padx=5, pady=15, ipadx=4, ipady=4)
 
 colon_label = tkinter.Label(time_frame, text=':', font=basic_font, fg='black')
@@ -38,10 +37,12 @@ sec_label = tkinter.Label(time_frame, textvariable=sec, font=basic_font, fg='bla
 sec.set('00')
 sec_label.grid(row=1, column=4, padx=5, pady=15, ipadx=4, ipady=4)
 
+stop_counter = False
+
 def Timer():
-    # global minute,second
+    global sec,min,stop_counter
+
     times=int(min.get())*60+int(sec.get())
-    print(times)
 
     while times > -1:
         minute, second=(times//60, times%60)
@@ -54,36 +55,48 @@ def Timer():
 
         if (times==0):
             playsound("bell.mp3")
-            second.set("00")
-            minute.set("00")
+            sec.set("00")
+            min.set("0")
+
+        if stop_counter:
+            stop_counter = False
+            return
 
         times -= 1
 
 def three():
-    min.set("03")
+    min.set("3")
     sec.set("00")
 
 def four():
-    min.set("04")
+    min.set("4")
     sec.set("00")
 
 def five():
-    min.set("05")
+    min.set("5")
     sec.set("00")
 
+def stop():
+    global sec,min,stop_counter
+    # start_flag = False
+    sec.set("00")
+    min.set("0")
+    time_frame.update()
+    stop_counter = True
+
 #時間設定
-stop_button = Button(time_select_frame, text='3分', command=three)
-stop_button.grid(row=1, column=1, padx=5, pady=20, ipadx=3, ipady=3)
-stop_button = Button(time_select_frame, text='4分', command=four)
-stop_button.grid(row=1, column=2, padx=5, pady=20, ipadx=3, ipady=3)
-stop_button = Button(time_select_frame, text='5分', command=five)
-stop_button.grid(row=1, column=3, padx=5, pady=20, ipadx=3, ipady=3)
+three_mins_button = Button(time_select_frame, text='3分', command=three)
+three_mins_button.grid(row=1, column=1, padx=5, pady=20, ipadx=3, ipady=3)
+four_mins_button = Button(time_select_frame, text='4分', command=four)
+four_mins_button.grid(row=1, column=2, padx=5, pady=20, ipadx=3, ipady=3)
+five_mins_button = Button(time_select_frame, text='5分', command=five)
+five_mins_button.grid(row=1, column=3, padx=5, pady=20, ipadx=3, ipady=3)
 
 #ボタン
 start_button = Button(button_frame, text='START', command=Timer)
 start_button.grid(row=1, column=1, padx=5, pady=20, ipadx=3, ipady=3)
-reset_button = Button(button_frame, text='STOP')
-reset_button.grid(row=1, column=2, padx=5, pady=20, ipadx=3, ipady=3)
+stop_button = Button(button_frame, text='STOP', command=stop)
+stop_button.grid(row=1, column=2, padx=5, pady=20, ipadx=3, ipady=3)
 
 # ウィンドウのループ処理
 root.mainloop()
